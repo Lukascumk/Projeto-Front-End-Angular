0342c65056196ab5273b1e27b6d581e58ecb2aad
Lukas Mota (Vs) <lukas_motta@outlook.com>
2024-06-07 01:18:40 -0300

    Criação de sidebar, navbar -  Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,18 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+
+
 
   ],
   imports: [
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { NavBarModule } from 'src/app/components/navbar/navbar.module';
+import { SideBarModule } from 'src/app/components/sidebar/sidebar.module';
 
 @NgModule(
   {
@@ -9,8 +11,10 @@ import { DashboardComponent } from './dashboard.component';
     declarations: [DashboardComponent],
     imports: [
       CommonModule,
-      DashboardRoutingModule
-        ],
+      DashboardRoutingModule,
+    NavBarModule,
+    SideBarModule
+        ]
 
     }
 )
