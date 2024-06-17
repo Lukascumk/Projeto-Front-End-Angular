e3b404bd08df75bf3bf70a25e5eaf5065e86fd6a
Lukas Mota (Vs) <lukas_motta@outlook.com>
2024-06-17 12:53:58 -0300

    Pagina de despesas criada

--- src/app/pages/despesa/despesa.module.ts
+++ src/app/pages/despesa/despesa.module.ts
@@ -4,6 +4,8 @@ import { NavBarModule } from 'src/app/components/navbar/navbar.module';
 import { SideBarModule } from 'src/app/components/sidebar/sidebar.module';
 import { DespesaComponent } from './despesa.component';
 import {  DespesaRoutingModule } from './despesa-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule(
   {
@@ -13,7 +15,10 @@ import {  DespesaRoutingModule } from './despesa-routing.module';
       CommonModule,
       DespesaRoutingModule,
     NavBarModule,
-    SideBarModule
+    SideBarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgSelectModule
         ]
 
     }
